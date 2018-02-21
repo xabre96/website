a210cd61247eec650377bdb8128547996ef8fc7c
xabre96 <xan.gutierrez@gmail.com>
2018-02-21 15:32:38 +0800

    Add middle initial.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Xan Gutierrez</h1>
+          <h1 className="App-title">Xan F. Gutierrez</h1>
         </header>
         <p className="App-intro">
           React + Github Pages = Awesomeness!!!
