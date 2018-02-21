045c146c9d286b0b4ac2a9b34080244546b24f4d
xabre96 <xan.gutierrez@gmail.com>
2018-02-21 14:59:21 +0800

    Change link.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <p className="App-intro">
           React + Github Pages = Awesomeness!!!
         </p>
-        <a href="/">https://github.com/xabre96/website</a>
+        <a href="/">https://xabre96.github.io/website</a>
       </div>
     );
   }
